3464480e78690a2d9b20fcd99df9064c97f2d551
Den Drobiazko <Den-Rimus@users.noreply.github.com>
2016-01-10 16:05:46 +0200

    Details presenter

---
+++ app/src/main/java/ua/dp/rename/dniprostreets/presenter/DetailsPresenter.java
@@ -0,0 +1,37 @@
+package ua.dp.rename.dniprostreets.presenter;
+
+import com.hannesdorfmann.mosby.mvp.MvpView;
+
+import ua.dp.rename.dniprostreets.core.BasePresenter;
+import ua.dp.rename.dniprostreets.entity.RenamedObject;
+
+public class DetailsPresenter extends BasePresenter<DetailsPresenter.View> {
+
+    private RenamedObject model;
+
+    public DetailsPresenter(RenamedObject model) {
+        this.model = model;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getView().syncUI(model);
+    }
+
+    public void detailsClicked() {
+        if (!model.hasLink()) return; // for safety
+        //
+        String url = model.getLink().getUrl();
+        if (!url.startsWith("http://") && !url.startsWith("https://")) url = "http://" + url;
+        //
+        getView().showDetails(url);
+    }
+
+    public interface View extends MvpView {
+
+        void syncUI(RenamedObject model);
+
+        void showDetails(String pageUrl);
+    }
+}
