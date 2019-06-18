ada1e4f1cdee7bde426b393464016cc1045463a7
Den Drobiazko <den.ddro@gmail.com>
2019-06-18 15:57:48 +0300

    Add extensions for views

---
+++ app/src/main/java/ua/dp/rename/dniprostreets/util/Extensions.kt
@@ -0,0 +1,16 @@
+package ua.dp.rename.dniprostreets.util
+
+import android.view.View
+import android.view.View.*
+
+fun View.gone() {
+   visibility = GONE
+}
+
+fun View.visible() {
+   visibility = VISIBLE
+}
+
+fun View.invisible() {
+   visibility = INVISIBLE
+}
